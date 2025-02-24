dda95228d39554ecdcb4ac25fb93c6aeb003f3f7
Jorge Martins <jorgemartins72@gmail.com>
2025-02-24 19:24:20 -0300

    config: CORS

--- infra/api/app.py
+++ infra/api/app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
 import os
 import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="datascore",
@@ -8,14 +9,22 @@ app = FastAPI(
     description="API DataScore",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/')
 def read_root():
     return {'message': "Ecobe"}
 
-@app.get('/testando')
-def teste():
-    env = dict()
-    for key, value in os.environ.items():
-        env[key] = value
+# @app.get('/testando')
+# def teste():
+#     env = dict()
+#     for key, value in os.environ.items():
+#         env[key] = value
 
-    return {'message': env}
+#     return {'message': env}
